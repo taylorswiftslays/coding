fe6c83854b9d9179912aad115657b1d4e8191c13
taylorswiftslays <157721562+taylorswiftslays@users.noreply.github.com>
2024-01-29 15:31:22 +0000

    done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,22 @@
 
+adverb = input("adverb ")
+
+bannana_color = input("bannana color? ")
+
+sport = input("Sport? ")
+
+treetype = input("tree type? ")
+
+animal = input("animal? ")
+
+country = input("Country? ")
+
+food = input("food? ")
+
+famousperson = input("Famous person? ")
+
+verb = input("verb? ")
+
+ocean = input("ocean? ")
+
+print (f"I {adverb} ate {bannana_color} bannans after a game of {sport} under a {treetype} tree. A {animal} was chasing after me in {country} Then I ate {food} and met {famousperson} I {verb} swam across the {ocean} ocean. Now you better leave otherwise😈😈! ")
